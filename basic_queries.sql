d9178bcf603cf9ce6fdf2f5bfd8169adcfd77417
Shridhar <shridhartapakire4051@gmail.com>
2026-05-27 17:35:35 +0530

    SQL: added INSERT, UPDATE, and DELETE queries

--- basic_queries.sql
+++ basic_queries.sql
@@ -13,3 +13,16 @@ WHERE salary > 50000;
 -- Sort data
 SELECT * FROM employees
 ORDER BY salary DESC;
+
+-- Insert new data
+INSERT INTO employees (id, name, salary)
+VALUES (1, 'Shridhar', 60000);
+
+-- Update existing data
+UPDATE employees
+SET salary = 70000
+WHERE id = 1;
+
+-- Delete data
+DELETE FROM employees
+WHERE id = 1;
