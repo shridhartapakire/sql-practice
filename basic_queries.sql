e2c36b33d9d5e538e526c585561bc88b2eb3368e
Shridhar <shridhartapakire4051@gmail.com>
2026-06-06 21:08:30 +0530

    SQL: added LEFT JOIN and RIGHT JOIN examples

--- basic_queries.sql
+++ basic_queries.sql
@@ -113,3 +113,22 @@ SELECT
 FROM employees
 INNER JOIN departments
 ON employees.department_id = departments.department_id;
+
+-- LEFT JOIN
+
+SELECT
+    employees.name,
+    departments.department_name
+FROM employees
+LEFT JOIN departments
+ON employees.department_id = departments.department_id;
+
+
+-- RIGHT JOIN
+
+SELECT
+    employees.name,
+    departments.department_name
+FROM employees
+RIGHT JOIN departments
+ON employees.department_id = departments.department_id;
